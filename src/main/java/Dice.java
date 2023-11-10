0aa365a583134de66cd918e65b0f2c3eacabdb95
Christy <christy@zipcoders-mbp-6.mynetworksettings.com>
2023-11-10 14:20:48 -0500

    Dice Done

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,23 @@
+import java.util.Random;
+
 public class Dice {
+    // create constructor (int numberOfDice)
+    int numberOfDice;
+    public Dice (int numberOfDice ){
+        this.numberOfDice = numberOfDice;
+    }
+    // create method tossAndSun()
+    // toss Dice & add up all the values
+    // return Integer
+
+    public Integer tossAndSum() {
+        Integer sum = 0;
+        Random random = new Random();
+
+        for (int i = 0; i < this.numberOfDice; i++) ;
+        sum += random.nextInt(6) + 1;
+        return sum;
+    }
 
 
 }
---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,26 @@
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DiceTest extends TestCase {
+@Test
+    public void testDiceConstruction(){
+    int numberOfDice = 2;
+    Dice dice = new  Dice(numberOfDice);
+
+    Assert.assertNotNull(dice);
+}
+
+@Test
+
+    public void testTossandSum (){
+    int numberOfDice = 2;
+    int max = numberOfDice * 6;//12
+    Dice dice = new Dice(numberOfDice);
+    Integer actual = dice.tossAndSum();
+
+    Assert.assertTrue(actual <= max);
+    Assert.assertTrue( actual >- numberOfDice;
+}
+
+}
